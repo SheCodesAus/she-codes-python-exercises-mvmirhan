0e47b61e2883ad4996a411dbbcb52f754b6b5a6a
mvmirhan <102584854+mvmirhan@users.noreply.github.com>
2022-04-28 21:51:43 +1000

    Created folder for exercises and completed exercise Q1 to Q4

--- 0_exercises/exercise_q3.py
+++ 0_exercises/exercise_q3.py
@@ -1,2 +1,6 @@
 # Write a program that takes a distance in kilometers from the user, 
 # and output the distance in meters and centimeters.
+
+a=float(input("enter distance in km : "))
+print("Distance in meters is ",int(a*1000))
+print("Distance in centimeters is ",int(a*1000*100))
---
+++ 0_exercises/exercise_q4.py
@@ -0,0 +1,8 @@
+# Write a program that takes the users name and height (in centimeters), 
+# and outputs a summary sentence.
+
+a=str(input("Enter your name : "))
+b=float(input("Enter your height in centimeters: "))
+print(a, "is " ,b ,"centimeters tall")
+
+
