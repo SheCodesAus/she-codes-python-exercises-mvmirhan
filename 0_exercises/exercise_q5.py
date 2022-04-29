7b59d0c077c8d59fc6f342f2fcc9fef3b2737b13
mvmirhan <102584854+mvmirhan@users.noreply.github.com>
2022-04-30 09:43:26 +1000

    Commit exercise Q5

---
+++ 0_exercises/exercise_q5.py
@@ -0,0 +1,7 @@
+#Ask user their name and then ask the user their age
+#Calculate how many years left for them to turn 90
+#Print "You have x year to turn 90"
+
+a=str(input("What is your name : "))
+b=int(input("How old are you? "))
+print (a, "You have" ,(90- b) ,"years to turn 90.")
