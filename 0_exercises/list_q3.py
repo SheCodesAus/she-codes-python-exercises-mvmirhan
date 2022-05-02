543fa353ca8dfa335d1b83a1d5ccf192ebe4302c
mvmirhan <mvmirhan@yahoo.com>
2022-05-02 19:41:30 +1000

    Updated list_q3.py

--- 0_exercises/list_q3.py
+++ 0_exercises/list_q3.py
@@ -1,8 +1,5 @@
 # Ask the user for three names, add them to a list, then print the lis
 
-from re import A
-
-
 name_1=input("Enter a name: ")
 name_2=input("Enter a name: ")
 name_3=input("Enter a name: ")
